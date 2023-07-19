b5ff93462778197f2471cf5f169559481059fd03
Felipe Martinez <felimartinez072@gmail.com>
2023-07-19 12:34:00 -0300

    new file:   .gitignore 	new file:   Program.cs 	new file:   bin/Debug/net7.0/HtmlAgilityPack.dll 	new file:   bin/Debug/net7.0/Newtonsoft.Json.dll 	new file:   bin/Debug/net7.0/prueba1.deps.json 	new file:   bin/Debug/net7.0/prueba1.dll 	new file:   bin/Debug/net7.0/prueba1.exe 	new file:   bin/Debug/net7.0/prueba1.pdb 	new file:   bin/Debug/net7.0/prueba1.runtimeconfig.json 	new file:   obj/Debug/net7.0/.NETCoreApp,Version=v7.0.AssemblyAttributes.cs 	new file:   obj/Debug/net7.0/apphost.exe 	new file:   obj/Debug/net7.0/prueba1.AssemblyInfo.cs 	new file:   obj/Debug/net7.0/prueba1.AssemblyInfoInputs.cache 	new file:   obj/Debug/net7.0/prueba1.GeneratedMSBuildEditorConfig.editorconfig 	new file:   obj/Debug/net7.0/prueba1.GlobalUsings.g.cs 	new file:   obj/Debug/net7.0/prueba1.assets.cache 	new file:   obj/Debug/net7.0/prueba1.csproj.AssemblyReference.cache 	new file:   obj/Debug/net7.0/prueba1.csproj.CopyComplete 	new file:   obj/Debug/net7.0/prueba1.csproj.CoreCompileInputs.cache 	new file:   obj/Debug/net7.0/prueba1.csproj.FileListAbsolute.txt 	new file:   obj/Debug/net7.0/prueba1.dll 	new file:   obj/Debug/net7.0/prueba1.genruntimeconfig.cache 	new file:   obj/Debug/net7.0/prueba1.pdb 	new file:   obj/Debug/net7.0/ref/prueba1.dll 	new file:   obj/Debug/net7.0/refint/prueba1.dll 	new file:   obj/project.assets.json 	new file:   obj/project.nuget.cache 	new file:   obj/prueba1.csproj.nuget.dgspec.json 	new file:   obj/prueba1.csproj.nuget.g.props 	new file:   obj/prueba1.csproj.nuget.g.targets 	new file:   prueba1.csproj

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+class Program
+{
+    static async System.Threading.Tasks.Task Main(string[] args)
+    {
+        Console.Write("Ingrese el ID del post de Reddit: ");
+        string postId = Console.ReadLine();
+
+        // Configurar el cliente HTTP con el User-Agent adecuado
+        using (HttpClient httpClient = new HttpClient())
+        {
+            httpClient.DefaultRequestHeaders.Add("User-Agent", "C# MyRedditApp/v1.0 (by /u/Jatemas)");
+
+            // Realizar la solicitud a la API de Reddit para obtener los comentarios
+            HttpResponseMessage response = await httpClient.GetAsync($"https://api.reddit.com/r/all/comments/{postId}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseBody = await response.Content.ReadAsStringAsync();
+                Console.WriteLine("Respuesta JSON:");
+                Console.WriteLine(responseBody);
+
+                // Procesar y mostrar los comentarios mejor votados
+                var comments = ParseComments(responseBody)
+                    .OrderByDescending(comment => comment.Votes)
+                    .Take(10);
+
+                foreach (var comment in comments)
+                {
+                    Console.WriteLine($"Votos: {comment.Votes}");
+                    Console.WriteLine($"Comentario: {comment.Text}");
+                    Console.WriteLine("-----------------------");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Error en la solicitud: {response.StatusCode} - {response.ReasonPhrase}");
+            }
+        }
+    }
+
+    static IEnumerable<Comment> ParseComments(string responseBody)
+    {
+        // Implementa la lógica para analizar y extraer los comentarios de la respuesta de la API de Reddit
+        // Puedes usar bibliotecas JSON como Newtonsoft.Json para facilitar el análisis
+
+        JObject jsonObject = JObject.Parse(responseBody);
+
+        JArray commentArray = jsonObject["data"]["children"].Value<JArray>();
+
+        foreach (JObject commentJson in commentArray)
+        {
+            JObject commentDataJson = commentJson["data"].Value<JObject>();
+            string text = commentDataJson["body"].ToString();
+            int votes = commentDataJson["ups"].ToObject<int>();
+            yield return new Comment { Text = text, Votes = votes };
+        }
+    }
+}
+
+class Comment
+{
+    public string? Text { get; set; }
+    public int Votes { get; set; }
+}
